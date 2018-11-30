1e1c180ee26fe924e8f9725d3dfea99ed72e7c15
Jason Rudy <jcrudy@gmail.com>
2018-11-29 17:29:13 -0800

    Improve the worker_id situation

--- deepq/environment/unity_adapter.py
+++ deepq/environment/unity_adapter.py
@@ -11,10 +11,22 @@ class UnityBasedEnvironment(Environment):
         Subclasses should set this to be the path to the 
         desired Unity environment.
         '''
-
+    
+    # It's necessary for UnityEnvironments to have unique worker_ids.  We can 
+    # ensure no two environments in a process share the same worker id by keeping
+    # a count.
+    worker_count = 0
+    
+    def __del__(self):
+        UnityBasedEnvironment.worker_count -= 1
+    
     def __init__(self, graphics=False):
+        # Attempt to make worker_id differ across processes.  Obviously not guaranteed,
+        # especially if each process has multiple workers.
+        worker_id = (os.getpid() % 100) + UnityBasedEnvironment.worker_count
         self.env = UnityEnvironment(file_name=self.path, no_graphics=(not graphics),
-                                    worker_id=os.getpid() % 100)
+                                    worker_id=worker_id)
+        UnityBasedEnvironment.worker_count += 1
         self.brain_name = self.env.brain_names[0]
         self.brain = self.env.brains[self.brain_name]
         env_info = self.env.reset(train_mode=True)[self.brain_name]
